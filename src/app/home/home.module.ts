f055ada061f4e1a283b2b50c05b52dd640f85718
caiomcs04 <caio6c@yahoo.com.br>
2021-07-21 03:30:11 -0300

    8#commit(implementação de validação atraves do componente)

--- src/app/home/home.module.ts
+++ src/app/home/home.module.ts
@@ -5,7 +5,7 @@ import { CommonModule } from '@angular/common';
 import { HomeRoutingModule } from './home-routing.module';
 import { HomeComponent } from './home.component';
 import { LoginComponent } from './login/login.component';
-import { FormsModule } from '@angular/forms';
+import { FormsModule, ReactiveFormsModule } from '@angular/forms';
 import { NewUserComponent } from './new-user/new-user.component';
 
 
@@ -19,7 +19,8 @@ import { NewUserComponent } from './new-user/new-user.component';
     CommonModule,
     HomeRoutingModule,
     FormsModule,
-    MenssageModule
+    MenssageModule,
+    ReactiveFormsModule
   ],
   exports: [
     HomeComponent
